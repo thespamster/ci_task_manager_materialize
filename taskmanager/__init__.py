70ec5324db257105c728986c83f60471c7857b5d
Gavin Brown <iamslowtrain@gmail.com>
2024-02-07 14:46:57 +0000

    correct an indentation error in init.py file.

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -11,9 +11,9 @@ if os.environ.get("DEVELOPMENT") == "TRUE":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-     if uri.startswith("postgres://"):
-         uri = uri.replace("postgres://", "postgresql://", 1)
-     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    if uri.startswith("postgres://"):
+      uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 db = SQLAlchemy(app)
 
 from taskmanager import routes  
